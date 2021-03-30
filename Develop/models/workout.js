ac733eca8c815c6b8bb718faad05e7f17d586418
paolorafaelyap <paolorafaelyap@gmail.com>
2021-03-29 21:44:42 -0700

    added schema

---
+++ Develop/models/workout.js
@@ -0,0 +1,45 @@
+//requiring mongoose
+const mongoose = require("mongoose");
+const schema = mongoose.Schema;
+
+// mongoose schema
+const workoutSchema = new schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true
+            },
+            name:{
+                type: String,
+                trim: true
+            },
+            duration: Number,
+            weight: {
+                type: Number,
+                default: 0
+            },
+            reps: {
+                type: Number,
+                default: 0
+            },
+            sets: {
+                type: Number,
+                default: 0
+            },
+            distance: {
+                type: Number,
+                default: 0
+            }
+        }
+    ],
+    totalDuration: {
+        type: Number,
+        default: 0,
+    }
+
+});
