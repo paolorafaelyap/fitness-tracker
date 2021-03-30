e7c8b37f9c3953dbc7a9ca484bccc21e181c43fd
paolorafaelyap <paolorafaelyap@gmail.com>
2021-03-29 22:05:11 -0700

    updated server.js

---
+++ Develop/server.js
@@ -0,0 +1,32 @@
+//dependencies
+const express = require("express");
+const mongoose = require("mogoose");
+//request logger middleware
+const logger = require("morgan");
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended:true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+//mongoose.connect command will try environment variable first, then do local if environment variable is not found.
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
+
+//the routes
+app.use(require("./routes/api.js"));
+app.use(require("./routes/html.js"));
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}..`);
+})
